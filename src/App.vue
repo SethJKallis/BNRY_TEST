2b0b38e7aef6f91750464fb12124b82ce9068f63
SethJKallis <kallisseth@gmail.com>
2023-04-20 16:28:49 +0200

    added a bootstrap nav bar

--- src/App.vue
+++ src/App.vue
@@ -18,12 +18,15 @@ components: {
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-
   color: #2c3e50;
+  margin-top: 4em;
 }
 
 nav {
   padding: 30px;
+  position: fixed;
+  top: 0;
+  width: 100%;
 }
 
 nav a {
--- src/components/NavComp.vue
+++ src/components/NavComp.vue
@@ -1,13 +1,40 @@
 <template>
     <div>
-        <nav>
-            <router-link to="/">Home Table</router-link> |
-            <router-link to="/wsj-articles">Wall Street Journal Articles</router-link> |
-            <router-link to="/tesla-articles">Tesla Articles</router-link> |
-            <router-link to="/us-business-articles">US Business Articles</router-link> |
-            <router-link to="/tech-crunch-articles">Tech Crunch Articles</router-link> |
-            <router-link to="/apple-articles">Apple Articles</router-link> |
-          </nav>     
+        <nav class="navbar bg-light">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="#">Navbar</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <router-link to="/">Home Table</router-link>
+                </li>
+                  <li class="nav-item">
+                    <router-link to="/wsj-articles">Wall Street Journal Articles</router-link>
+                </li>
+                <li>
+                    <router-link to="/tesla-articles">Tesla Articles</router-link>
+                </li>
+                <li>
+                    <router-link to="/us-business-articles">US Business Articles</router-link>
+                </li>
+                <li>
+                    <router-link to="/tech-crunch-articles">Tech Crunch Articles</router-link>
+                </li>
+                <li>
+                    <router-link to="/apple-articles">Apple Articles</router-link>
+                </li>
+                   
+                </ul>
+                <form class="d-flex" role="search">
+                  <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                  <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+              </div>
+            </div>
+          </nav>  
 
     </div>
 </template>
@@ -17,3 +44,12 @@ export default{
     name: 'NavComp',
 }
 </script>
+
+<style scoped>
+nav{
+    z-index: 1;
+    position: fixed;
+    top: 0;
+    width: 100%;
+}
+</style>
